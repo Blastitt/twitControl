05f8c2b4e3f0548e03eae19500001954a78ebcd9
Blastitt <lazarjonah@gmail.com>
2016-07-13 11:49:40 -0400

    Initial commit

---
+++ node_helper.js
@@ -0,0 +1,46 @@
+
+var NodeHelper = require("node_helper");
+var Stream = require('user-stream');
+
+module.exports = NodeHelper.create({
+	
+	start: function() {
+		console.log("Starting module: " + this.name);
+		
+	},
+	
+	socketNotificationReceived: function(notification, payload) {
+		
+		console.log("Notification: " + notification + " Payload: " + payload);
+		
+		if(notification === "START_STREAM"){
+			this.startTwitterStream(payload.config.api_keys);
+		}
+		
+	},
+	
+	startTwitterStream: function(api_keys){
+		
+		var self = this;
+		
+		this.stream = new Stream({
+			consumer_key: api_keys.consumer_key,
+			consumer_secret: api_keys.consumer_secret,
+			access_token_key: api_keys.access_token_key,
+			access_token_secret: api_keys.access_token_secret
+		});
+		
+		this.params = {
+			with: 'user',
+			stringify_friend_ids: true
+		};
+		
+		this.stream.stream(this.params);
+		
+		this.stream.on('data', function(json) {
+		  self.sendSocketNotification("DATA", json);
+		});
+	},
+
+
+});
---
+++ twitControl.js
@@ -0,0 +1,65 @@
+Module.register("twitControl",{
+
+	// Default module config.
+	defaults: {
+		api_keys: {
+			consumer_key: '',
+			consumer_secret: '',
+			access_token_key: '',
+			access_token_secret: ''
+		},
+	},
+	
+	start: function() {
+		Log.info(this.config);
+		Log.info("Starting module: " + this.name);
+		this.startStream();
+	},
+	
+	// Define required scripts.
+	getScripts: function() {
+		return ["moment.js"];
+	},
+	
+	startStream: function(){
+		Log.info(this.config);
+		this.tweets = [];
+		this.sendSocketNotification("START_STREAM", {config: this.config});
+	},
+	
+	socketNotificationReceived: function(notification, payload){
+		if(notification === "DATA"){
+			if(payload.text != null){
+				payload.date = new Date();
+				this.tweets.push(payload);
+				this.updateDom(2.5 * 1000);
+			}
+		}
+	},
+
+	// Override dom generator.
+	getDom: function() {
+		var wrapper = document.createElement("div");
+		var title = document.createElement("div");
+		title.className = "small light bright";
+		title.innerHTML = "Twitter Feed";
+		wrapper.appendChild(title);
+		for(var t in this.tweets){
+			
+				var tweetElement = document.createElement("div");
+				tweetElement.className = "bright medium light compact";
+				tweetElement.innerHTML = this.tweets[t].text;
+				
+				var author = document.createElement("div");
+				author.className = "small light";
+				author.innerHTML = "@"+this.tweets[t].user.screen_name;
+				//author.innerHTML += " | " + this.tweets[t].date.prototype.toLocaleString();
+				
+				
+				wrapper.appendChild(tweetElement);
+				wrapper.appendChild(author);
+		}
+		return wrapper;
+	}
+});
+
